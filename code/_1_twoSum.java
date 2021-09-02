23217909906b5c855022ba79ea50d9d0367830b1
zpfff9603 <mf1932242@smail.nju.edu.cn>
2021-09-02 20:23:21 +0800

    提交

---
+++ code/_1_twoSum.java
@@ -0,0 +1,20 @@
+package code;
+
+import java.util.HashMap;
+
+public class _1_twoSum {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{i, map.get(target - nums[i])};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        System.out.println("aadf");
+    }
+}
